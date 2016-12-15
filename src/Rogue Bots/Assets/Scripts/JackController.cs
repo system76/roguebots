273b474806b466a563d3d2b91ea0a88d52f67137
David Brooks <skade88@gmail.com>
2016-12-14 20:11:15 -0600

    Updated unity project to 5.6.0xb. Converted one eyed jack to a fbx. Imported fbx into unity project. Created basic script to allow jack to move in the scene.

---
+++ src/Rogue Bots/Assets/Scripts/JackController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JackController : MonoBehaviour {
+
+	public float moveSpeed = 10f;
+	public float turnSpeed = 50f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.W))
+			transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.S))
+			transform.Translate (-Vector3.forward * moveSpeed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.A))
+			transform.Translate (Vector3.right * moveSpeed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.D))
+			transform.Translate (Vector3.left * moveSpeed * Time.deltaTime);	
+		
+	}
+}
